0b037ea497e4e8ecacf896dada26b8178cca8270
Omar Ahmed <oa48879@gmail.com>
2021-04-30 13:27:47 +0200

    config editing

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import {sequelize} from './sequelize';
 
 import {FeedRouter} from './controller/v0/feed/routes/feed.router';
@@ -18,22 +18,27 @@ import {V0_FEED_MODELS} from './controller/v0/model.index';
 
   app.use(bodyParser.json());
 
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
-  }));
+  // app.use(cors({
+  //   allowedHeaders: [
+  //     'Origin', 'X-Requested-With',
+  //     'Content-Type', 'Accept',
+  //     'X-Access-Token', 'Authorization',
+  //   ],
+  //   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  //   origin: config.url,
+  // }));
+  app.use(cors());
+  app.get('/health', async (req: Request, res: Response) => {
+    // Health check
+    res.status(200).send('All clear')
+  });
 
   app.use('/api/v0/feed', FeedRouter);
 
   // Root URI call
-  app.get( '/', async ( req, res ) => {
-    res.send( '/api/v0/' );
-  } );
+  // app.get( '/', async ( req, res ) => {
+  //   res.send( '/api/v0/' );
+  // } );
 
 
   // Start the Server
