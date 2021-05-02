3d6ff6fcaedd2ad2fce61e45a095fbbd3a23deb3
Omar Ahmed <oa48879@gmail.com>
2021-05-02 06:39:19 +0200

    remove cores

--- src/server.ts
+++ src/server.ts
@@ -18,15 +18,15 @@ import {V0_FEED_MODELS} from './controller/v0/model.index';
 
   app.use(bodyParser.json());
 
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: config.url,
-  }));
+  // app.use(cors({
+  //   allowedHeaders: [
+  //     'Origin', 'X-Requested-With',
+  //     'Content-Type', 'Accept',
+  //     'X-Access-Token', 'Authorization',
+  //   ],
+  //   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  //   origin: config.url,
+  // }));
   // app.use(cors());
   app.get('/health', async (req: Request, res: Response) => {
     // Health check
